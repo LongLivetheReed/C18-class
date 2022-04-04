e93e0c92dae6e9db5586c78ce28b333a43d80969
LongLivetheReed <87645804+LongLivetheReed@users.noreply.github.com>
2022-04-04 17:58:07 +1000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var s1,s2
+function setup() {
+  createCanvas(400,400);
+s1 = new Student("Leon",10,16);
+s1.display();
+s2 = new Student("Tom",11,17);
+s2.display();
+}
+
+function draw() 
+{
+  background(30);
+  
+}
